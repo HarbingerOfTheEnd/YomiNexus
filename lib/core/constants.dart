f74805dd49d0b2b02c591638487000fc55892564
Sai Kumar <ksai44576@gmail.com>
2024-09-13 22:21:08 +0530

    Add the NovelLibraryView route

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:yominexus/ui/library/novel/novel_library_view.dart';
 
 @immutable
 class Constants {
   const Constants._();
 
-  static final Map<String, WidgetBuilder> routes = {};
+  static final Map<String, WidgetBuilder> routes = {
+    NovelLibraryView.routeName: (context) => const NovelLibraryView(),
+  };
 }
---
+++ lib/ui/library/novel/novel_library_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class NovelLibraryView extends ConsumerWidget {
+  const NovelLibraryView({super.key});
+
+  static const String routeName = '/library/novel';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
