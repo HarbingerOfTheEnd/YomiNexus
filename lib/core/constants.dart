dd2b829b5181e23cba3e89c7180ced3f8ea48055
Sai Kumar <ksai44576@gmail.com>
2024-09-20 13:25:48 +0530

    Add shared_preferences key string and a colorscheme

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -9,4 +9,43 @@ class Constants {
   static final Map<String, WidgetBuilder> routes = {
     NovelLibraryView.routeName: (context) => const NovelLibraryView(),
   };
+
+  static const String _keyPrefix = 'yominexus';
+  static const String themeDataKey = '$_keyPrefix.themeData';
+  static const String themeModeKey = '$_keyPrefix.themeMode';
+  static const Set<String> allowList = {
+    themeDataKey,
+    themeModeKey,
+  };
+
+  static final ThemeData cloudflareColorSchemeDark = ThemeData.dark(
+    useMaterial3: true,
+  ).copyWith(
+    colorScheme: const ColorScheme(
+      brightness: Brightness.dark,
+      primary: Color(0xFFF38020),
+      onPrimary: Color(0xFF1B1B22),
+      primaryContainer: Color(0xFFF38020),
+      onPrimaryContainer: Color(0xFF1B1B22),
+      inversePrimary: Color(0xFFD6BAFF),
+      secondary: Color(0xFFF38020),
+      onSecondary: Color(0xFF1B1B22),
+      secondaryContainer: Color(0xFFF38020),
+      onSecondaryContainer: Color(0xFF1B1B22),
+      tertiary: Color(0xFF1B1B22),
+      onTertiary: Color(0xFFF38020),
+      tertiaryContainer: Color(0xFF1B1B22),
+      onTertiaryContainer: Color(0xFFF38020),
+      surface: Color(0xFF1B1B22),
+      onSurface: Color(0xFFEFF2F5),
+      surfaceContainerHighest: Color(0xFF3F3F46),
+      onSurfaceVariant: Color(0xFFD8FFFF),
+      surfaceTint: Color(0xFFF38020),
+      inverseSurface: Color(0xFFF3EFF4),
+      onInverseSurface: Color(0xFF313033),
+      outline: Color(0xFFF38020),
+      error: Color(0xFFCF6679),
+      onError: Color(0xFF1C1B1F),
+    ),
+  );
 }
