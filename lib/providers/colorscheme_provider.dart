1b2fa21273856c64777c4b63e517b3d8d104637a
Sai Kumar <ksai44576@gmail.com>
2024-09-27 22:57:54 +0530

    Add ColorschemeProvider class and its implementation
    This commit adds the ColorschemeProvider class and its implementation. The ColorschemeProvider is a riverpod provider that retrieves the color scheme from the shared preferences. It also provides a method to set the color scheme. This class is used to manage the color scheme in the application.

---
+++ lib/providers/colorscheme_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:yominexus/core/constants.dart';
+import 'package:yominexus/core/extensions.dart';
+
+part 'colorscheme_provider.g.dart';
+
+@riverpod
+class ColorschemeProvider extends _$ColorschemeProvider {
+  @override
+  (ColorScheme, ColorScheme) build() {
+    return Constants.sharedPreferences.getColorScheme();
+  }
+
+  Future<void> setColorScheme(ColorScheme colorscheme) async {
+    state = await Constants.sharedPreferences.setColorScheme(colorscheme);
+  }
+}
---
+++ lib/providers/colorscheme_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'colorscheme_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$colorschemeProviderHash() =>
+    r'af775922a258a655d6f33aaf4b3ec4738718c2fe';
+
+/// See also [ColorschemeProvider].
+@ProviderFor(ColorschemeProvider)
+final colorschemeProviderProvider = AutoDisposeNotifierProvider<
+    ColorschemeProvider, (ColorScheme, ColorScheme)>.internal(
+  ColorschemeProvider.new,
+  name: r'colorschemeProviderProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$colorschemeProviderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ColorschemeProvider = AutoDisposeNotifier<(ColorScheme, ColorScheme)>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
