900c94f536e39a64cca2e2f6c293bf2e33f56c95
Sai Kumar <ksai44576@gmail.com>
2024-09-27 22:09:23 +0530

    Refactor color scheme retrieval in SharedPreferencesWithCache extension

--- lib/core/extensions.dart
+++ lib/core/extensions.dart
@@ -2,18 +2,93 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yominexus/core/constants.dart';
 
-extension GetThemeData on SharedPreferencesWithCache {
-  ThemeData getThemeData() {
-    final String? themeData = getString(Constants.themeDataKey);
-    final String? themeMode = getString(Constants.themeModeKey);
-
-    if (themeData == null || themeMode == null) return ThemeData.light();
-
-    switch ([themeData, themeMode]) {
-      case ['CloudflareColorScheme', 'dark']:
-        return Constants.cloudflareColorSchemeDark;
-      default:
-        return ThemeData.light();
+extension ColorSchemeExt on SharedPreferencesWithCache {
+  (ColorScheme, ColorScheme) getColorScheme() {
+    final String? colorschemeValue = getString(Constants.colorschemeKey);
+    if (colorschemeValue == null) {
+      return (
+        Constants.defaultColorSchemeLight,
+        Constants.defaultColorSchemeDark
+      );
     }
+
+    return switch (colorschemeValue) {
+      'Default' => (
+          Constants.defaultColorSchemeLight,
+          Constants.defaultColorSchemeDark,
+        ),
+      'Dynamic' => (
+          Constants.dynamicColorSchemeLight,
+          Constants.dynamicColorSchemeDark,
+        ),
+      'Cloudflare' => (
+          Constants.cloudflareColorSchemeLight,
+          Constants.cloudflareColorSchemeDark,
+        ),
+      'Cotton Candy' => (
+          Constants.cottonCandyColorSchemeLight,
+          Constants.cottonCandyColorSchemeDark,
+        ),
+      'Doom' => (
+          Constants.doomColorSchemeLight,
+          Constants.doomColorSchemeDark,
+        ),
+      'Green Apple' => (
+          Constants.greenAppleColorSchemeLight,
+          Constants.greenAppleColorSchemeDark,
+        ),
+      'Lavender' => (
+          Constants.lavenderColorSchemeLight,
+          Constants.lavenderColorSchemeDark,
+        ),
+      'Matrix' => (
+          Constants.matrixColorSchemeLight,
+          Constants.matrixColorSchemeDark,
+        ),
+      'Midnight Dusk' => (
+          Constants.midnightDuskColorSchemeLight,
+          Constants.midnightDuskColorSchemeDark,
+        ),
+      'Mocha' => (
+          Constants.mochaColorSchemeLight,
+          Constants.mochaColorSchemeDark,
+        ),
+      'Sapphire' => (
+          Constants.sapphireColorSchemeLight,
+          Constants.sapphireColorSchemeDark,
+        ),
+      'Nord' => (
+          Constants.nordColorSchemeLight,
+          Constants.nordColorSchemeDark,
+        ),
+      'Strawberry Daiquiri' => (
+          Constants.strawberryDaiquiriColorSchemeLight,
+          Constants.strawberryDaiquiriColorSchemeDark,
+        ),
+      'Tako' => (
+          Constants.takoColorSchemeLight,
+          Constants.takoColorSchemeDark,
+        ),
+      'Teal & Turquoise' => (
+          Constants.tealAndTurquoiseColorSchemeLight,
+          Constants.tealAndTurquoiseColorSchemeDark,
+        ),
+      'Tidal Wave' => (
+          Constants.tidalWaveColorSchemeLight,
+          Constants.tidalWaveColorSchemeDark,
+        ),
+      'Yin & Yang' => (
+          Constants.yinAndYangColorSchemeLight,
+          Constants.yinAndYangColorSchemeDark,
+        ),
+      'Yotsuba' => (
+          Constants.yotsubaColorSchemeLight,
+          Constants.yotsubaColorSchemeDark,
+        ),
+      _ => (
+          Constants.defaultColorSchemeLight,
+          Constants.defaultColorSchemeDark,
+        ),
+    };
   }
 }
