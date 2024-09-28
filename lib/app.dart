74a06a617cd89a911ae0a1f6eafbd40aa318d75a
Sai Kumar <ksai44576@gmail.com>
2024-09-28 11:41:33 +0530

    Refactor theme provider to use dynamic theme in App widget

--- lib/app.dart
+++ lib/app.dart
@@ -3,12 +3,16 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yominexus/core/constants.dart';
+import 'package:yominexus/providers/theme_provider.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final (lightTheme, darkTheme) = ref.watch(yominexusThemeProvider);
+    final ThemeData? theme = lightTheme ?? darkTheme;
+
     return MaterialApp(
       restorationScopeId: 'app',
       localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
@@ -22,8 +26,8 @@ class App extends ConsumerWidget {
       ],
       onGenerateTitle: (BuildContext context) =>
           AppLocalizations.of(context)?.appTitle ?? 'YomiNexus',
-      theme: ThemeData(),
-      darkTheme: ThemeData.dark(),
+      theme: theme,
+      darkTheme: darkTheme,
       initialRoute: Constants.initialRoute,
       routes: Constants.routes,
       debugShowCheckedModeBanner: false,
