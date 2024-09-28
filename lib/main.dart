df02e69160b3cbe1e6fb6b38453988bd4a5dba02
Sai Kumar <ksai44576@gmail.com>
2024-09-28 11:24:13 +0530

    Initialize late variables in `Constants` class

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yominexus/core/constants.dart';
 
 import 'app.dart';
 
 void main() async {
+  await Constants.init();
   runApp(
     const ProviderScope(
       child: App(),
